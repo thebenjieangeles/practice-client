691f86c20457c9db2c070e29b91f15cdf20f5f81
thebenjieangeles <zerofourtwentifive@gmail.com>
2023-08-04 10:44:19 +0700

    fixed bugs

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
   server: {
-    proxy: "http://localhost:8080",
+    proxy: "http://localhost:10000",
   },
 });
